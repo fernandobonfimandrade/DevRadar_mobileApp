e56168e37f5fc8302e73ed60735ce37b3c869cbf
Fernando Bonfim Andrade <fernandobonfimandrade@gmail.com>
2020-01-29 23:48:45 -0300

    incluido marker,calout,  githubpage, and search form

--- src/routes.js
+++ src/routes.js
@@ -20,6 +20,7 @@ const Routes = createAppContainer(
         }
     },{
         defaultNavigationOptions:{
+            headerBackTitleVisible: false,
             headerTintColor: '#fff',
             headerStyle:{
                 backgroundColor: '#7d40e7',
